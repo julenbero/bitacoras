20be63c76794e972959270f6a551a31b09c81f72
julenbero <julenbero@gmail.com>
2018-12-19 20:20:25 -0500

    Creacion Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EjecuciontComponent } from './components/ejecuciont/ejecuciont.component';
+import { EjecuciondComponent } from './components/ejecuciond/ejecuciond.component';
+import { RegistrosComponent } from './components/registros/registros.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '**', redirectTo: 'Home', pathMatch: 'full' },     
+     { path: 'Home', component: HomeComponent },
+     { path: 'EjecucionDiaria', component: EjecuciondComponent },
+     { path: 'EjecucionTotal', component: EjecuciontComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { EjecuciontComponent } from './components/ejecuciont/ejecuciont.component';
 import { EjecuciondComponent } from './components/ejecuciond/ejecuciond.component';
 import { RegistrosComponent } from './components/registros/registros.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EjecuciontComponent,
     EjecuciondComponent,
-    RegistrosComponent
+    RegistrosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
